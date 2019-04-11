b79866e5f4750bc421024b8146893e167d1c88d7
Sai Vinay <32012516+saivinayg@users.noreply.github.com>
2019-04-11 16:09:01 -0700

    Clean Coding

--- src/view/student/QuizSubmissionAcknowledgement.java
+++ src/view/student/QuizSubmissionAcknowledgement.java
@@ -1,8 +1,10 @@
 package view.student;
-
+/*
+author: Sai Vinay Gangadharabhatla.
+version: 1.0.
+*/
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -14,26 +16,7 @@ import java.awt.event.ActionEvent;
 public class QuizSubmissionAcknowledgement extends JFrame {
 
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	/*public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					QuizCreationAcknowledgement frame = new QuizCreationAcknowledgement();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}*/
-
-	/**
-	 * Create the frame.
-	 */
+	//Create the Submission Acknowledgement frame.
 	public QuizSubmissionAcknowledgement() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
