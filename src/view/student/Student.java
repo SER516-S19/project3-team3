e0868ec165ff19269230fd4385dc371b4b8b5114
gaddevmounika <46894014+gaddevmounika@users.noreply.github.com>
2019-04-11 16:19:06 -0700

    Clean Coding

--- src/view/student/Student.java
+++ src/view/student/Student.java
@@ -3,11 +3,11 @@ package view.student;
 import java.awt.*;
 import javax.swing.*;
 /**
- * SelectQuizView will display the available quizzes 
+ *  To display the list of available quizzes 
  * @author Vijaya Gadde
  *
  */
-public class SelectQuizView  {
+public class Student  {
 	JFrame frame = new JFrame("Student Quiz Application");
 	JPanel panel = new JPanel();
 	String quizzes[] = { "QuizFinal", "QuizFinal1", "QuizFinal2" };
@@ -16,7 +16,7 @@ public class SelectQuizView  {
 	JLabel pageName = new JLabel("Student Dashboard");
 	JLabel selectQuizName = new JLabel("Select Quiz"); 
 
-	public SelectQuizView() {
+	public Student() {
 		panel.setLayout(new GridLayout(4,1,2,30));
 		panel.add(pageName);
 		panel.add(selectQuizName);
@@ -29,6 +29,6 @@ public class SelectQuizView  {
 		frame.setVisible(true);
 	}
 	public static void main(String[] args) {
-		new  SelectQuizView();
+		new  Student();
 	}
 }
