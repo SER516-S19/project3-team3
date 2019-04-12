ea8b48a3b49e5f82bc8c472b12ae1cdb78b91c00
Sakshi Gautam <sakshi7373@gmail.com>
2019-04-11 18:34:09 -0700

    Removing blank lines

--- src/view/professor/Professor.java
+++ src/view/professor/Professor.java
@@ -3,20 +3,15 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.event.*;
-
 /*
  *@author: Sakshi Gautam
  *@Description: Professor application to create and edit quiz.
  *@version: 1.1 
  */
-
 public class Professor extends JFrame {
 	private JFrame profFrame;
 	private JPanel contentPane;
-
-	/*
-	 * Launch the application.
-	 */
+	 // Launch the application.
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -29,8 +24,6 @@ public class Professor extends JFrame {
 			}
 		});
 	}
-
-
 	public Professor() {
 		profFrame = new JFrame("Professor Application");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -40,22 +33,18 @@ public class Professor extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		profFrame.setSize(500,500);
-
 		JLabel welcomeLabel = new JLabel("Welcome Professor!");
 		welcomeLabel.setBounds(120, 98, 226, 50);
 		welcomeLabel.setFont(welcomeLabel.getFont().deriveFont(20.0f));
 		contentPane.add(welcomeLabel);
-		
 		JButton createQuizButton = new JButton("Create Quiz");
 		createQuizButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				CreateNewQuiz createQuiz=new CreateNewQuiz();
 				setVisible(false); // Hide current frame
-				createQuiz.setVisible(true);
-		       
+				createQuiz.setVisible(true);       
 			}
 		});
-
 		createQuizButton.setBounds(30, 197, 180, 23);
 		contentPane.add(createQuizButton);
 	}
